088ddaf7cfdb2f7d9519a16f031c4827c7030605
mohamedkamalhaamad <mohamedkamalhammad2022@gmail.com>
2025-09-16 16:41:40 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -11,7 +11,6 @@ window.onscroll = function() {
     }
 }
 
-
 // add class active to nav_Links
 
 let nav_Links = document.getElementById("nav_links");
@@ -19,3 +18,19 @@ let nav_Links = document.getElementById("nav_links");
 function op_cl_Menu() {
     nav_Links.classList.toggle("active")
 }
+
+window.addEventListener('DOMContentLoaded', () => {
+    // اختر العناصر التي تريد تطبيق الانيميشن عليها
+    const homeText = document.querySelector('.hero .div_text');
+    const projectsSection = document.querySelector('.projects');
+
+    // أضف الكلاس الابتدائي
+    homeText.classList.add('animate-slide-down');
+    projectsSection.classList.add('animate-slide-down');
+
+    // بعد قليل، أضف الكلاس الذي يفعّل الانيميشن
+    setTimeout(() => {
+        homeText.classList.add('active');
+        projectsSection.classList.add('active');
+    }, 100); // تأخير بسيط للسماح للمتصفح بتطبيق الحالة الابتدائية
+});
